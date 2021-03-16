a438e3007d839c9c55463446733a545a4d9c1b3d
Cris Coelho <nunujunior30@gmail.com>
2021-03-16 18:15:22 -0300

    Signed-off-by: Cris Coelho <nunujunior30@gmail.com>

---
+++ PrimeiroProjeto/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 2;
+            int b = 4;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
